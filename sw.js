e40baeb3637ce9f61e68b40691159c2466ae3fd0
comboxvalencia1-pixel <combox.valencia1@gmail.com>
2026-03-02 18:37:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,38 @@
-const CACHE_NAME = 'combox-v3';
+const CACHE_NAME = 'combox-cache-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700;800;900&display=swap'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700;800;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
+// Instalación: Guarda los archivos esenciales en la memoria del iPad
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+// Activación: Limpia cachés viejas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Primero busca en internet, si falla, usa la caché
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });

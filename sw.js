67cc5c97407f925186e6ddb10a7b535b9008b724
comboxvalencia1-pixel <combox.valencia1@gmail.com>
2026-03-03 06:27:15 -0400

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,38 +1,21 @@
-const CACHE_NAME = 'combox-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700;800;900&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
-];
+const CACHE_NAME = "combox-pro-v5";
+const urlsToCache = ["./", "./index.html", "./style.css", "./script.js", "./manifest.json"];
 
-// Instalación: Guarda los archivos esenciales en la memoria del iPad
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener("install", event => {
+    self.skipWaiting();
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activación: Limpia cachés viejas
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener("activate", event => {
+    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
 
-// Estrategia: Primero busca en internet, si falla, usa la caché
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request).then(res => {
+            const clone = res.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+            return res;
+        }).catch(() => caches.match(event.request))
+    );
 });
